27f52253a44fcd4fbf08f0dd9e132eac9ef4f2a1
Jamesy-tech <jamesycreator@gmail.com>
2026-07-13 07:56:05 +1200

    Add YouTube subscription automation script
    This script automates the process of subscribing to a YouTube channel using a Raspberry Pi Pico 2 W. It opens a browser, navigates to the subscription link, and then closes the tab.

---
+++ pico-scripts/subscribe.py
@@ -0,0 +1,60 @@
+# Made for Raspberry Pi Pico 2 W using Circuit Python [should work with most other models]
+# This script quickly opens YouTube, subscribes to the desired channel and closes it
+# code.py
+
+import time
+import board
+import digitalio
+import usb_hid
+
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+keyboard = Keyboard(usb_hid.devices)
+layout = KeyboardLayoutUS(keyboard)
+
+time.sleep(0)
+
+led.value = True
+time.sleep(0.15)
+led.value = False
+
+keyboard.release_all()
+time.sleep(0.1)
+
+keyboard.press(Keycode.CONTROL)
+keyboard.press(Keycode.T)
+time.sleep(0.05)
+keyboard.release_all()
+
+time.sleep(0.5)
+
+layout.write("https://www.youtube.com/@jamesydeveloper?sub_confirmation=1")
+
+keyboard.send(Keycode.ENTER)
+keyboard.release_all()
+
+time.sleep(3)
+
+keyboard.send(Keycode.TAB)
+keyboard.release_all()
+time.sleep(0.5)
+keyboard.send(Keycode.TAB)
+keyboard.release_all()
+keyboard.send(Keycode.ENTER)
+keyboard.release_all()
+
+time.sleep(0.5)
+
+keyboard.press(Keycode.CONTROL)
+keyboard.press(Keycode.W)
+
+time.sleep(0.3)
+
+keyboard.release_all()
+
+time.sleep(100000)

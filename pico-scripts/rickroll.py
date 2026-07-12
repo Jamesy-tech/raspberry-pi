035e8b771b7941abbd213ee1c3aeb55dd76aed75
Jamesy-tech <jamesycreator@gmail.com>
2026-07-12 17:55:03 +1200

    Add rickroll script with LED and keyboard control
    This script blinks an LED and opens a web browser to a specific URL.

---
+++ pico-scripts/rickroll.py
@@ -0,0 +1,36 @@
+import time
+import board
+import digitalio
+import usb_hid
+
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
+
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+keyboard = Keyboard(usb_hid.devices)
+layout = KeyboardLayoutUS(keyboard)
+
+time.sleep(0)
+
+led.value = True
+time.sleep(0.15)
+led.value = False
+
+keyboard.release_all()
+time.sleep(0.1)
+
+keyboard.press(Keycode.CONTROL)
+keyboard.press(Keycode.T)
+time.sleep(0.05)
+keyboard.release_all()
+
+time.sleep(0.5)
+
+layout.write("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+time.sleep(0.1)
+
+keyboard.send(Keycode.ENTER)

24975ebcfdf20f851c35383d16af34fdda4622f8
Jamesy-tech <jamesycreator@gmail.com>
2026-07-14 15:51:49 +1200

    Refactor boot.py by removing comments and updating print
    Removed comments related to USB drive status and adjusted print statement for clarity.

--- pico-scripts/DuckyScript/boot.py
+++ pico-scripts/DuckyScript/boot.py
@@ -8,9 +8,7 @@ noStoragePin.switch_to_input(pull=digitalio.Pull.UP)
 noStorageStatus = not noStoragePin.value
 
 if(noStorageStatus == True):
-    # don't show USB drive to host PC
     storage.disable_usb_drive()
     print("Disabling USB drive")
 else:
-    # normal boot
     print("USB drive enabled")
